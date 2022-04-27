cd01bfbc362de00fea7f51f471a67cc19377e1dd
Enzo Holgado <103465174+Zoen-DEV@users.noreply.github.com>
2022-04-26 20:45:35 -0500

    .

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+// Importante: los estilos, deben ir debajo
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
---
+++ mecanografia.js
@@ -0,0 +1,40 @@
+/*
+asdf ñlkj asdf ñlkj asdf ñlkj asdf asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf 
+asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf
+asdf ñlkj asdf ñlkj asdf ñlkj asdf asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf lkj asdfñlkj asdflkj sadflkj sdñlj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
